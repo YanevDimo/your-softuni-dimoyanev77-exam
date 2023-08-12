19a3aa3524a900179d857c7625fc273a6b10fb68
Dimo Yanev <dimocz@gamail.com>
2023-08-12 15:10:20 +0200

    change ui

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://dimoyanev77.onrender.com');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
